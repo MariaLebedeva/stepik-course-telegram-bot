cda516a908f3b0a71cfe376a6c3d7353143ca6ed
maria <maria.v.lebedeva@gmail.com>
2020-11-14 19:31:26 +0300

    re write the bot to show tasks

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ token = '325273800:AAGrT1sh8FREiy2i_Xss865c9tJDP3rRLzg'
 
 bot = telebot.TeleBot(token)
 
-score = {'victories': 0, 'defeats': 0}
+states = {}
+
+MAIN_STATE = 'main'
+TASK_DATE_STATE = 'task_date'
 
 
 def say_hello(user_name, lang_code):
@@ -15,24 +18,19 @@ def say_hello(user_name, lang_code):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Это бот-тренажер ударений. Он поможет научиться правильной постановке ударений в сложных "
-                          "словах.")
+    bot.reply_to(message, "Это бот-планировщик. Он поможет сориентироваться в твоих задачах.")
 
 
 @bot.message_handler(func=lambda message: True)
-def content(message):
+def dispatcher(message):
     if message.text == "Привет!":
         bot.reply_to(message, say_hello(message.from_user.first_name, message.from_user.language_code))
-    elif message.text == "Спроси меня слово":
-        bot.reply_to(message, "Как правильно - Искра или искрА?")
-    elif message.text == "Искра":
-        bot.reply_to(message, "Правильно!")
-        score['victories'] += 1
-    elif message.text == "искрА":
-        bot.reply_to(message, "Неправильно :(")
-        score['defeats'] += 1
-    elif message.text == "Покажи счет":
-        bot.reply_to(message, str(score['victories']) + " : " + str(score['defeats']))
+    elif message.text == "Покажи задачи":
+        bot.reply_to(message, "Уточни дату")
+    elif message.text == "Сегодня":
+        bot.reply_to(message, "На сегодня 2 задачи")
+    elif message.text == "Завтра":
+        bot.reply_to(message, "Нет задач")
     else:
         bot.reply_to(message, "Я тебя не понял")
 
